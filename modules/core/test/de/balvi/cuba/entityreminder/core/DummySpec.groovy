7749842bf89d2da9d7eef7d1893d00b7442c2676
ha <csh.shop@googlemai.com>
2017-04-26 11:10:23 +0200

    added default settings (groovy, spock, codenarc etc)

---
+++ modules/core/test/de/balvi/cuba/entityreminder/core/DummySpec.groovy
@@ -0,0 +1,11 @@
+package de.balvi.cuba.entityreminder.core
+
+import spock.lang.Specification
+
+class DummySpec extends Specification {
+
+    def "true is not false"() {
+        expect:
+        true
+    }
+}
